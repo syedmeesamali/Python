7693763c24ea4ae86fbaa875c434cace5841a0fc
syedmeesamali <syedmeesamali@yahoo.com>
2018-09-09 09:26:57 +0400

    moving weighted average completed with while loop

--- Previous/moving_weighted_avg.py
+++ Previous/moving_weighted_avg.py
@@ -15,17 +15,18 @@ def moving_window_average(input_list, n_neighbors=1):
     # x = [x[0]]*n_neighbors + x + [x[-1]]*n_neighbors
     # To complete the function,
     # return a list of the mean of values from i to i+width for all values i from 0 to n-1.
-    for i in range(n):
-        if i==0:
-            avg = (input_list[i]*2 + input_list[i+1])/3
-            result_list.append(avg)
-        elif i==n-1:
-            avg = (input_list[i]*2 + input_list[i-1])/3
-            result_list.append(avg)
-        else:
-            avg = (input_list[i] + input_list[i-1] + input_list[i-1])/3
-            result_list.append(avg)
+    i=0
+    avg = round((input_list[i]*2 + input_list[i+1])/3,2)
+    result_list.append(avg)
+    while i < n-2:
+        i = i + 1
+        avg = round((input_list[i-1] + input_list[i] + input_list[i+1])/3,2)
+        result_list.append(avg)
+    i = i + 1
+    avg = round((input_list[i]*2 + input_list[i-1])/3,2)
+    result_list.append(avg)
     return result_list
 
 x=[0,10,5,3,1,5]
 print(moving_window_average(x,1))
+   
