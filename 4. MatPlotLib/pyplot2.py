903d2ba2b081128f1ef06c4c03b8cff8a03905da
syedmeesamali <syedmeesamali@yahoo.com>
2018-11-09 23:59:14 +0400

    Logarithmic plots

--- 4. MatPlotLib/pyplot2.py
+++ 4. MatPlotLib/pyplot2.py
@@ -3,17 +3,17 @@ import numpy as np
 plt.plot([0,1,4,9,16])
 
 x = np.linspace(0,10,20)
+x2 = np.logspace(-1,1,40)
 y = x**2.0
 #plt.plot(x,y)
 #plt.show()
 
-y1 = x**2.0
-y2 = x**1.5
-plt.plot(x,y1,"bo-",linewidth=2,markersize=12, label="First")
-plt.plot(x,y2,"gs-",linewidth=2,markersize=12, label="Second")
+y1 = x2**2.0
+y2 = x2**1.5
+plt.loglog(x2,y1,"bo-",linewidth=2,markersize=12, label="First")
+plt.loglog(x2,y2,"gs-",linewidth=2,markersize=12, label="Second")
 plt.xlabel("$X$")
 plt.ylabel("$Y$")
 plt.axis([-0.5, 10.5, -5, 105])
 plt.legend(loc="upper left")
-plt.savefig("myplot.pdf")
 plt.show()
