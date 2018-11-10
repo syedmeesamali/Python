cd3b54694d89ded0f78c185370ccf0b2ca6abfc3
syedmeesamali <syedmeesamali@yahoo.com>
2018-11-10 23:54:14 +0400

    Histogram plotting

--- 4. MatPlotLib/pyplot2.py
+++ 4. MatPlotLib/pyplot2.py
@@ -1,17 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
-plt.plot([0,1,4,9,16])
 
-x = np.linspace(0,10,20)
-x2 = np.logspace(-1,1,40)
-y = x**2.0
-#plt.plot(x,y)
-#plt.show()
+x = np.logspace(-1,1,40)
+y = x**2
 
-y1 = x2**2.0
-y2 = x2**1.5
-plt.loglog(x2,y1,"bo-",linewidth=2,markersize=12, label="First")
-plt.loglog(x2,y2,"gs-",linewidth=2,markersize=12, label="Second")
+y1 = x**2.0
+y2 = x**1.5
+plt.loglog(x,y1,"bo-",linewidth=2,markersize=12, label="First")
+plt.loglog(x,y2,"gs-",linewidth=2,markersize=12, label="Second")
 plt.xlabel("$X$")
 plt.ylabel("$Y$")
 plt.axis([-0.5, 10.5, -5, 105])
