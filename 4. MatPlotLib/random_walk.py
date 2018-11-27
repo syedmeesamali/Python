f9b8f2a757cfe82522f9bdb2e9430337c88e56df
syedmeesamali <syedmeesamali@yahoo.com>
2018-11-27 22:30:56 +0400

    Random walk using numpy and plt

---
+++ 4. MatPlotLib/random_walk.py
@@ -0,0 +1,12 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+#X_0 will give 0,0 as the origin of the random walker
+X_0 = np.array([[0],[0]])
+delta_X = np.random.normal(0,1,(2,100))
+
+X = np.concatenate((X_0, np.cumsum(delta_X, axis=1)), axis=1)
+plt.plot(X[0], X[1], "ro-")
+plt.savefig("rw.pdf")
+plt.show()
