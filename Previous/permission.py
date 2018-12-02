1e95f8603663bc0ff1ac01f10dc3e38641a3125e
syedmeesamali <syedmeesamali@yahoo.com>
2018-12-02 15:07:41 +0400

    Python teaching (lecture - 1) variables, if condition, functional programming basics

---
+++ Previous/permission.py
@@ -0,0 +1,5 @@
+def permission(age):
+    if age > 30:
+        return "Permission Granted!"
+    else:
+        return "Permission DENIED"
---
+++ Previous/quadratic.py
@@ -0,0 +1,2 @@
+def quadratic(x):
+    return abs(x**2 - 4*x + 1)
