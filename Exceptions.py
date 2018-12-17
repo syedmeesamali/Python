eb603a58ac768730a4822794b281f017f346c9a9
syedmeesamali <syedmeesamali@yahoo.com>
2018-12-17 23:09:18 +0400

    Working example

--- Exceptions.py
+++ Exceptions.py
@@ -1,7 +1,6 @@
-a = input("Enter string only")
-try:
-    print("String entered")
-except:
-    ValueError
-else:
-    print("No error")
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
