3c6c1678a8f7eef1f296e084ffa6ab1c6f57f7ac
syedmeesamali <syedmeesamali@yahoo.com>
2018-12-24 16:59:23 +0400

    Pandas example # 3 from inbox course - Random pandas books

---
+++ 5. Pandas/pandas_lec-3.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy.random as np
+import random
+import sys
+import matplotlib
+
+#print("Python version " + sys.version)
+#print("Pandas version " + pd.__version__)
+#print("Matplotlib version " + matplotlib.__version__)
+
+random.seed(111)
+
+def CreateDataSet(Number = 1):
+    Output = []
+    for i in range(Number):
+        rng = pd.date_range(start = '1/1/2009', end='12/31/2012', freq='W-MON')
+
+        data = np.randint(25,999,size=len(rng))
+        status = [1,2,3]
+        random_status = [status[np.randint(0, len(status))] for i in range(len(rng))]
+
+        states = ['GA', 'FL', 'fl', 'NY', 'NJ', 'TX']
+        random_states = [states[np.randint(0,len(states))] for i in range(len(rng))]
+
+        Output.extend(zip(random_states, random_status, data, rng))
+    return Output
+                                
