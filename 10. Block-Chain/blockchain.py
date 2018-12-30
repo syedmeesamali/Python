96141e6c25c65d0a30655b6b7529a2dfd10590b7
syedmeesamali <syedmeesamali@yahoo.com>
2018-12-31 02:07:44 +0400

    Using SHA-256 function from python's SHA256 library

--- 10. Block-Chain/blockchain.py
+++ 10. Block-Chain/blockchain.py
@@ -7,11 +7,28 @@ class BlockChain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+
+        #create the genesis block
         self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self):
-        #Create a new block here
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """Creates a new block in the Blockchain
+
+        :param proof: <int> The proof given by proof of work (PoW) algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+            }
+
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """Creates a new transaction to go into next block
@@ -33,9 +50,22 @@ class BlockChain(object):
         #Hashes of blocks
         pass
 
+    @property
     def last_block(self):
         #returns last block in the chain as of now
-        pass
+        return self.chain[-1]
+
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    
 
     
     
