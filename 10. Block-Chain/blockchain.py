81a1c0493b30dab450d3e6ccbfda48de9439a09e
syedmeesamali <syedmeesamali@yahoo.com>
2018-12-30 17:16:18 +0400

    Building a blockchain in Python - Medium article

---
+++ 10. Block-Chain/blockchain.py
@@ -0,0 +1,25 @@
+class BlockChain(object):
+
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+
+    def new_block(self):
+        #Create a new block here
+        pass
+
+    def new_transaction(self):
+        #New transactions to be added here
+        pass
+
+    def hash(block):
+        #Hashes of blocks
+        pass
+
+    def last_block(self):
+        #returns last block in the chain as of now
+        pass
+
+    
+    
