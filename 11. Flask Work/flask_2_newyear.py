2653553e3df17b617c22e956b9404375925c4c32
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-04 19:23:44 +0400

    Example working perfect now. Index HTML should be in new templates folder

--- 11. Flask Work/flask_2_newyear.py
+++ 11. Flask Work/flask_2_newyear.py
@@ -3,12 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/main")
 def index():
     now = datetime.datetime.now()
     #check if today is the new year
     new_year = now.month == 1 and now.day == 1
+    new_year = True
     return render_template("index.html", new_year = new_year)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
