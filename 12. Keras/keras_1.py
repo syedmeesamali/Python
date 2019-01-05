81307deb2fe3fb200b7d53063173f4f7cb128b59
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-06 01:12:57 +0400

    First complete keras example

--- 12. Keras/keras_1.py
+++ 12. Keras/keras_1.py
@@ -1,2 +1,22 @@
+import pandas as pd
 import keras
+
+concrete_data = pd.read_csv('https://ibm.box.com/shared/static/svl8tu7cmod6tizo6rk0ke4sbuhtpdfx.csv')
+concrete_data.shape
+predictors = concrete_data.iloc[:,0:8]
+target =concrete_data.iloc[:,8]
+
 from keras.models import Sequential
+from keras.layers import Dense
+
+model = Sequential()
+n_cols = concrete_data.shape[1]
+
+model.add(Dense(5, activation='relu', input_shape=(n_cols,)))
+model.add(Dense(5, activation='relu'))
+model.add(Dense(1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(predictors, target)
+
+predictions = model.predict(test_data)
