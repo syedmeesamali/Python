bc2bba28dd3b2f68354c794103303883f9a2ddbd
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-10 16:45:57 +0400

    Addition of the more option page

--- 11. Flask Work/flask_3_loops.py
+++ 11. Flask Work/flask_3_loops.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/main")
+@app.route("/")
 def index():
     names = ['ali', 'jennoy', 'mary', 'lihn']
     return render_template("index.html", names = names)
+
+@app.route("/more")
+def index():
+    return render_template("more.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
