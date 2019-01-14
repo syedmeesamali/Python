1ee0481a98fdc2d6954415db0c6015b915f8eb93
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-15 02:01:29 +0400

    Updated direct path for engine variable, installed psycopg2 using pip and now program perfectly works and Python is connecting to Database.......

--- 11. Flask Work/flask_sql_1.py
+++ 11. Flask Work/flask_sql_1.py
@@ -3,14 +3,14 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine("postgres://postgres:admin@localhost:5432")
 
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
     flights = db.execute("SELECT origin, destination, duration FROM flights2").fetchall()
     for flight in flights:
-        printf(f"{flight.origin} to {flights.destination}, {flights.duration}")
+        print(f"{flight.origin} to {flight.destination}, {flight.duration}")
         
 
 if __name__ == "__main__":
