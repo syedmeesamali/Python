55a51310bc83d2ad3985e9315747c8d500197c60
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-17 17:02:41 +0400

    App working perfectly and updating the database from the HTML page via Flask

--- 11. Flask Work/flask_sql_App.py
+++ 11. Flask Work/flask_sql_App.py
@@ -13,6 +13,22 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     flights = db.execute("SELECT * FROM flights2").fetchall()
     return render_template("index.html", flights = flights)
+
+@app.route("/book", methods=["POST"])
+def book():
+    name = request.form.get("name")
+    #Book a new flight
+    try:
+        flight_id = int(request.form.get("flight_id"))
+    except ValueError:
+        return render_template("error.html", message="Invalid flight number")
+
+    if db.execute("SELECT * FROM flights2 WHERE id= :id", {"id": flight_id}).rowcount == 0:
+        render_template("error.html", message = "No flights with that ID")
+    db.execute("INSERT INTO passengers (name, flight_id) VALUES (:name, :flight_id)",
+               {"name": name, "flight_id": flight_id})
+    db.commit()
+    return render_template("success.html")
         
 if __name__ == "__main__":
     app.run(debug=True)
