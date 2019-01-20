07afb4de33486d49f137f47edb8a2a8138d6a1fc
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-20 16:54:11 +0400

    Start with first HEROKU project and some updates to flight class

--- 11. Flask Work/Class_Flight.py
+++ 11. Flask Work/Class_Flight.py
@@ -5,13 +5,24 @@ class Flight:
         self.destination = destination
         self.duration = duration
 
-def main():
-    f = Flight(origin="New York", destination="Paris", duration=540)
-    f.duration += 10
+    def print_info(self):
+        print(f"Flight Origin: {self.origin}")
+        print(f"Flight Destination: {self.destination}")
+        print(f"Flight Duration: {self.duration}")
 
-    print(f.origin)
-    print(f.destination)
-    print(f.duration)
+    def delay(self, amount):
+        self.duration += amount
+
+def main():
+    f1 = Flight(origin="New York", destination="Paris", duration=540)
+    f1.print_info()
+    f1.delay(25)
+    f1.print_info()
+
+    f2 = Flight(origin="Tokyo", destination="Shanghai", duration=140)
+    f2.print_info()
+    f2.delay(10)
+    f2.print_info()
 
 if __name__ == "__main__":
     main()
