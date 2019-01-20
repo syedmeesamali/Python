d9a61f44f15c39327f67ddcdd685160ed6c9e38e
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-20 22:29:15 +0400

    Connection of Heroku Postgres

--- 14. Project_1/project1/application.py
+++ 14. Project_1/project1/application.py
@@ -6,19 +6,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
-
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+engine = create_engine("postgres://qutsooplqiicse:d60da7c9883bd2eba6ce3507550dd0e11ef8afa7f4a6557beac7eb79f16208e5@ec2-54-235-68-3.compute-1.amazonaws.com:5432/d1cpp71n0aft0")
+db = scoped_session(sessionmaker(bind=engine))
+                       
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
