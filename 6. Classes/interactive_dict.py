6bce05c10c3a3fdfd82ef58286f62387f7913270
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-21 15:25:36 +0400

    Dictionary program updated to accept words capital or small

--- 6. Classes/interactive_dict.py
+++ 6. Classes/interactive_dict.py
@@ -3,8 +3,17 @@ data = json.load(open('dictionary.json'))
 
 #function to retrieve defintion of any word
 def retrieve_definition(word):
+    #Removing the case-sensitivity from the program
+    #For example 'Rain' and 'rain' will give same output
+    #Converting all letters to lower because out data is in that format
+    word = word.lower()
+
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     else:
         return ("The word doesn't exist in dictionary")
 
