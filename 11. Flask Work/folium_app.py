1ba92ce412fd002542446bec58cf62a8232360a8
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-23 17:28:07 +0400

    Creating models file for connecting SQL directly with Flask app using SQLAlchemy

--- 11. Flask Work/folium_app.py
+++ 11. Flask Work/folium_app.py
@@ -1,7 +1,16 @@
 import folium
+import pandas as pd
 
-map = folium.Map(location=[37.296933,-121.9574983], zoom_start=8, tiles = "Mapbox bright")
+#Load data of volcanoes in USA
+data = pd.read_csv('C:/Users/SYED/Desktop/Python/11. Flask Work/data/Volcanoes_USA.txt')
+lat = data['LAT']
+lon = data['LON']
+elevation = data['ELEV']
 
-folium.Marker(location=[37.296933,-121.9574983], popup='Google HQ', icon=folium.Icon(color='blue')).add_to(map)
+#Create base map
+map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 5, tiles = "Mapbox bright")
+
+for lat, lon, elevation in zip(lat, lon, elevation):
+    folium.Marker(location=[lat, lon], popup=str(elevation)+" m", icon=folium.Icon(color = 'gray')).add_to(map)
 
 map.save("map1.html")
---
+++ 11. Flask Work/models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Flight(db.Model):
+    __tablename__ = "flights"
+    id = db.Column(db.Integer, primary_key = True)
+    origin = db.Column(db.String, nullable = False)
+    destination = db.Column(db.String, nullable = False)
+    duration = db.Column(db.Integer, nullable = False)
+    
+class Passenger(db.Model):
+    __tablename__ = "passengers"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable = False)
+    flight_id = db.Column(db.Integer, db.Foreignkey("flights.id"), nullable = False)
+
+
+def main():
+    db.create_all()
+
+if __name__ == "__main__":
+    with app.app_context():
+        main()
