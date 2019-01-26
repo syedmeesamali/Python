387a9479d4cac79497cd25016c8f7f1d28979e4a
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-26 17:46:08 +0400

    Finding outliers of data using two methods

--- 5. Pandas/pandas_lec-7.py
+++ 5. Pandas/pandas_lec-7.py
@@ -18,3 +18,20 @@ df2['State'] = States
 #Combine the two dataframes
 df = pd.concat([df1, df2])
 print(df)
+
+
+#Ways to calculate the outliers
+#Method-1: make a copy of original df
+newdf = df.copy()
+newdf['X-Mean'] = abs(newdf['Revenue']-newdf['Revenue'].mean())
+newdf['1.96*std'] = 1.96*newdf['Revenue'].std()
+newdf['Outlier'] = abs(newdf['Revenue'] - newdf['Revenue'].mean()) > 1.96*newdf['Revenue'].std()
+#print(newdf)
+
+#Method-2: Group by item
+newdf2 = df.copy()
+State = newdf2.groupby('State')
+newdf2['Outlier'] = State.transform( lambda x: abs(x-x.mean()) > 1.96*x.std() )
+newdf2['x-Mean'] = State.transform( lambda x: abs(x-x.mean()) )
+newdf['1.96*std'] = State.transform( lambda x: 1.96*x.std() )
+print(newdf2)
