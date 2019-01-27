d61de8a5e74b051824c8afd8ebe06b17e641071d
syedmeesamali <syedmeesamali@yahoo.com>
2019-01-28 03:29:40 +0400

    Beautiful final map of volcanoes in USA using Folium package

--- 11_Flask Work/folium_app.py
+++ 11_Flask Work/folium_app.py
@@ -1,5 +1,6 @@
 import folium
 import pandas as pd
+from folium.plugins import MarkerCluster
 
 #Load data of volcanoes in USA
 data = pd.read_csv('C:/Users/SYED/Desktop/Python/11_Flask Work/data/Volcanoes.txt')
@@ -7,10 +8,23 @@ lat = data['LAT']
 lon = data['LON']
 elevation = data['ELEV']
 
+#Function to change colors
+def color_change(elev):
+    if(elev < 1000):
+        return('green')
+    elif(1000 <= elev < 3000):
+        return('orange')
+    else:
+        return('red')
+
 #Create base map
-map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 5, tiles = "Mapbox bright")
+map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 5, tiles = "CartoDB dark_matter")
+
+#Create cluster
+marker_cluster = MarkerCluster().add_to(map)
 
 for lat, lon, elevation in zip(lat, lon, elevation):
-    folium.Marker(location=[lat, lon], popup=str(elevation)+" m", icon=folium.Icon(color = 'gray')).add_to(map)
-
+    folium.CircleMarker(location=[lat, lon], radius=9, popup=str(elevation)+" m",
+                  fill_color=color_change(elevation), color="gray", fill_opacity=0.9).add_to(marker_cluster)
+    
 map.save("map1.html")
