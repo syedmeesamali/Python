10f770e0fc72b768cc06923fd3f5d5fae3e4899d
syedmeesamali <syedmeesamali@yahoo.com>
2019-02-23 17:42:12 +0400

    Updates to the data collector App

--- 4_Flask/Data_Scrapper/data_collector_App.py
+++ 4_Flask/Data_Scrapper/data_collector_App.py
@@ -1,7 +1,7 @@
 #Import dependencies
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask.ext.sqlalchemy import SQLAlchemy
+#from flask.ext.sqlalchemy import SQLAlchemy
 
 #Create instance of flask app
 app = Flask(__name__)
@@ -9,6 +9,21 @@ app = Flask(__name__)
 #Connect to the Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:admin@localhost:5432'
 db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    #Create a new table
+
+    __table_name__ = "data"
+    id = db.Column(db.Integer, primary_key = True)
+    height = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
+    sex = db.Column(db.String)
+
+    def __init__(self, height, weight, sex):
+        self.height = height
+        self.weight = weight
+        self.sex = sex
 
 #Define route and content of page
 @app.route("/")
