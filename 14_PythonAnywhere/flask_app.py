176d5745d7524e62947cf7bf6f603fe5a44ab990
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-06 02:35:11 +0400

    Database setup using the SQLAlchemy

--- 14_PythonAnywhere/flask_app.py
+++ 14_PythonAnywhere/flask_app.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMRY_DATABASE_URI'] = 'postgres://postgres:admin@localhost:5432'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80), unique = True)
+    email = db.Column(db.String(120), unique = True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return "<{0} id: {1}>".format(self.__class__.__name__, self.id)
 
 @app.route("/")
 def index():
