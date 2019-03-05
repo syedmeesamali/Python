3beb128f0d7aa059b1c65bdf8dd639a56c5b1de6
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-06 02:20:05 +0400

    restarting work with flask from scratch

--- 14_PythonAnywhere/flask_app.py
+++ 14_PythonAnywhere/flask_app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/<string:name>")
-def index(name):
-    return f"Hello, {name}"
+@app.route("/")
+def index():
+    return "<h1 style='color: red'>Hello, Ali Shah</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
