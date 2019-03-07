e7b039e04fa1d466fa80f751d13dd6256c2cef55
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-07 23:27:27 +0400

    After lot of pain and struggle finally the local app is working........Hurrah !!!

--- 14_PythonAnywhere/flask_app.py
+++ 14_PythonAnywhere/flask_app.py
@@ -3,21 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import render_template, request, redirect, url_for
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgres://postgres:admin@localhost/postgres'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:admin@localhost/movie'
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    __table_name__ = "entry"
+class Movie(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String)
-    email = db.Column(db.String)
+    username = db.Column(db.String(80))
+    email = db.Column(db.String(120))
 
     def __init__(self, username, email):
         self.username = username
         self.email = email
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Movie %r>' % self.username
 
 @app.route('/')
 def index():
@@ -26,8 +26,8 @@ def index():
 @app.route('/post_user', methods=['POST'])
 def post_user():
     if(request.method == 'POST'):
-        user = User(request.form['username'], request.form['email'])
-        db.session.add(user)
+        user1 = Movie(request.form['username'], request.form['email'])
+        db.session.add(user1)
         db.session.commit()
         return redirect(url_for('index'))
 
