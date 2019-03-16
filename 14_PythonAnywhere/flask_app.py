d75fbb7e31d181b5b362e117161cc042ef7b4479
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-16 20:09:54 +0400

    Some definitions for flask-security

--- 14_PythonAnywhere/flask_app.py
+++ 14_PythonAnywhere/flask_app.py
@@ -25,7 +25,21 @@ roles_users = db.Table('roles_users', db.Column('user_id', db.Integer(), db.Fore
                 db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
 class Role(db.Model, Rolemixin):
-    
+    id = db.Column(db.Integer(), primary_key = True)
+    name = db.Column(db.String(80), unique = True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, Usermixin):
+    id = db.Column(db.Integer(), primary_key = True)
+    email = db.Column(db.String(255), unique = True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary = roles_users, backref = db.backref('users', 'lazy = dynamic'))
+
+#Flask-security settings
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
 @app.route('/')
 def index():
