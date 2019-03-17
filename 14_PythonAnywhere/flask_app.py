aeff9b45bdacc9aeeb9913efcc11d92001680baf
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-17 17:31:36 +0400

    flask app with flask-security

--- 14_PythonAnywhere/flask_app.py
+++ 14_PythonAnywhere/flask_app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import render_template, request, redirect, url_for
-from flask_security import security, SQLAlchemyUserDatastore, Usermixin, Rolemixin, login_required
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:admin@localhost/movie'
+app.config['SECRET-KEY'] = 'super-secret'
+app.config['SECURITY_REGISTERABLE'] = True
+app.debug = True
 db = SQLAlchemy(app)
 
 # class Movie(db.Model):
@@ -20,41 +23,49 @@ db = SQLAlchemy(app)
 #     def __repr__(self):
 #         return '<Movie %r>' % self.username
 
-#Define new models for our flask-security database
-roles_users = db.Table('roles_users', db.Column('user_id', db.Integer(), db.ForeignKey('user.id')), 
-                db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+# Define models
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
-class Role(db.Model, Rolemixin):
-    id = db.Column(db.Integer(), primary_key = True)
-    name = db.Column(db.String(80), unique = True)
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
-class User(db.Model, Usermixin):
-    id = db.Column(db.Integer(), primary_key = True)
-    email = db.Column(db.String(255), unique = True)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship('Role', secondary = roles_users, backref = db.backref('users', 'lazy = dynamic'))
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
 
-#Flask-security settings
+# Setup Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
+# Create a user to test with
+@app.before_first_request
+def create_user():
+    db.create_all()
+    user_datastore.create_user(email='matt@nobien.net', password='password')
+    db.session.commit()
+
 @app.route('/')
 def index():
-    myUser = Movie.query.all()
-    return render_template("add_user.html", myUser = myUser)
+    return render_template("add_user.html")
 
 @app.route('/profile/<username>')
 def profile(username):
-    user = Movie.query.filter_by(username = username).first()
+    user = User.query.filter_by(email = username).first()
     return render_template("profile.html", user = user)
 
 @app.route('/post_user', methods=['POST'])
 def post_user():
     if(request.method == 'POST'):
-        user1 = Movie(request.form['username'], request.form['email'])
+        user1 = User(request.form['username'], request.form['email'])
         db.session.add(user1)
         db.session.commit()
         return render_template("success.html")
