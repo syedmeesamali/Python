42ecbcfa62ceab6cc369131d50beb88579862d9b
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-28 12:16:53 +0400

    Automation work in Python - starting with emailing

--- 14_PythonAnywhere/flask_app.py
+++ 14_PythonAnywhere/flask_app.py
@@ -24,13 +24,13 @@ class Role(db.Model, RoleMixin):
     description = db.Column(db.String(255))
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
+    id = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(200))
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
+                            backref=db.backref('user', lazy='dynamic'))
 
 # Setup Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
---
+++ 15_Automation/emails_1.py
@@ -0,0 +1,3 @@
+import smtplib
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+print(type(smtpObj))
