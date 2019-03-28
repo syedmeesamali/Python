f341bd5f7d00e842fd836e06bf1e8c6228081dda
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-28 14:42:20 +0400

    Program working perfectly now with Outlook smtp server

--- 15_Automation/emails_1.py
+++ 15_Automation/emails_1.py
@@ -1,3 +1,12 @@
 import smtplib
-smtpObj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-print(type(smtpObj))
+server = smtplib.SMTP('smtp-mail.outlook.com:587')
+print(type(server))
+server.ehlo()
+server.starttls()
+try:
+    server.login("my_email_address", "my_password")
+except SMTPAuthenticationError:
+    print("MTPAuthenticationError")
+server.sendmail('my_email_address', 'recipient_Email', 'Subject: Email from python')
+print("Email sent successfully")
+server.quit()
