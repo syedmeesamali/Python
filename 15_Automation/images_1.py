9a8050c8512668e8d8debe1d845c0e794cf1879c
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-30 17:55:21 +0400

    creating and manipulating image files

--- 15_Automation/images_1.py
+++ 15_Automation/images_1.py
@@ -1,2 +1,10 @@
 import os
 from PIL import Image
+loveImg = Image.open('heart.jpg')
+print(loveImg.size)
+print(loveImg.filename)
+
+im = Image.new('RGBA', (100, 200), 'purple')
+im.save('purpleImage.png')
+im2 = Image.new('RGBA', (20, 20))
+im2.save('transparent.png')
