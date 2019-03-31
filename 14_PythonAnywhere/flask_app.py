ecaa677965484756f2f9dfd64238e6f0c3fa99d6
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-31 15:13:43 +0400

    App working now without flask-security. Need to make this work for pythonanywhere

--- 14_PythonAnywhere/flask_app.py
+++ 14_PythonAnywhere/flask_app.py
@@ -14,27 +14,12 @@ roles_users = db.Table('roles_users',
         db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
         db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
-class Role(db.Model, RoleMixin):
+class Role(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    email = db.Column(db.String(200), unique=True)
-    password = db.Column(db.String(200))
-    active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('user', lazy='dynamic'))
-
-# Setup Flask-Security
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security(app, user_datastore)
-
-# Views
 @app.route('/')
-@login_required
 def home():
     return render_template('add_user.html')
 
