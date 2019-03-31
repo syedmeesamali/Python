bcf271f9cdffd53bd6ef63b7d47a24793d66e110
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-31 11:40:30 +0400

    Updates to the app to utilize online free hosted database

--- 14_PythonAnywhere/flask_app.py
+++ 14_PythonAnywhere/flask_app.py
@@ -8,7 +8,7 @@ from flask_security import Security, SQLAlchemyUserDatastore, \
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'super-secret'
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:admin@localhost/movie'
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://minhkibb:f8-3TYEbK21CZKJJdDb8EaLob8iSLs4r@balarama.db.elephantsql.com:5432/minhkibb'
 app.config['SECURITY_PASSWORD_SALT'] = 'secret'
 # Create database connection object
 db = SQLAlchemy(app)
