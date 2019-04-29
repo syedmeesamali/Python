6b7cf54cd971c6ed69f163c90df45f4c3e46f630
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-29 13:11:59 +0400

    BOT working fine sending random jpeg images

--- 11_BOT/my_telegram_bot.py
+++ 11_BOT/my_telegram_bot.py
@@ -13,3 +13,14 @@ def bop(bot, update):
     url = get_url()
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
+
+
+def main():
+    updater = Updater('my_token')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop',bop))
+    updater.start_polling()
+    updater.idle()
+    
+if __name__ == '__main__':
+    main()
