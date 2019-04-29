2b77cd297e6f75b5e4a3e2cfb56f12eec2df00a2
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-29 13:03:58 +0400

    Telegram BOT

---
+++ 11_BOT/my_telegram_bot.py
@@ -0,0 +1,15 @@
+from telegram.ext import Updater, CommandHandler
+import requests
+import re
+contents = requests.get('https://random.dog/woof.json').json()
+image_url = contents['url']
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
