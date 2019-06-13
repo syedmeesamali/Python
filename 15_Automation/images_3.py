ddea407630a563265092403eddca5e855448356f
syedmeesamali <syedmeesamali@yahoo.com>
2019-06-13 15:29:51 +0400

    Image resizing using python's PIL

---
+++ 15_Automation/images_3.py
@@ -0,0 +1,11 @@
+import os
+from PIL import Image
+loveImg = Image.open('heart.jpg')
+print(loveImg.size)
+print(loveImg.filename)
+
+width, height = loveImg.size
+quarterSize = loveImg.resize((int(width/2), int(height/2)))
+quarterSize.save('quarterLove.png')
+saveItem = ((width, height+200))
+saveItem.save('saveItem.png')
