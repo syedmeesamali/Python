3556ed01dc6c19a5aec41b2b360a303d666779c8
syedmeesamali <syedmeesamali@yahoo.com>
2019-06-15 17:20:07 +0400

    image rotations

--- 15_Automation/images_3.py
+++ 15_Automation/images_3.py
@@ -7,5 +7,6 @@ print(loveImg.filename)
 width, height = loveImg.size
 quarterSize = loveImg.resize((int(width/2), int(height/2)))
 quarterSize.save('quarterLove.png')
+quarterSize.rotate(90).save('rotated.png')
 saveItem = loveImg.resize((width, height+200))
 saveItem.save('saveItem.png')
