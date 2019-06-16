64e67b505d0b9c0c66106a9e101cc276ceb3c075
syedmeesamali <syedmeesamali@yahoo.com>
2019-06-16 23:47:31 +0400

    small mistake corrected

--- 15_Automation/images_4.py
+++ 15_Automation/images_4.py
@@ -6,7 +6,7 @@ for x in range(100):
     for y in range(50):
         im.putpixel((x,y),(210,210,210))
 
-for PIL import ImageColor
+from PIL import ImageColor
 for x in range(100):
     for y in range(50,100):
         im.putpixel((x,y), ImageColor.getcolor('darkgrey','RGBA'))
