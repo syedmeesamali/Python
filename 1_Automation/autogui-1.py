e872334e3e7e655a283fcbb86079619b45ff7a18
syedmeesamali <syedmeesamali@yahoo.com>
2019-09-23 10:27:42 +0400

    Amazing gui and resource automation

--- 1_Automation/autogui-1.py
+++ 1_Automation/autogui-1.py
@@ -1,3 +1,6 @@
 import pyautogui
-print(pyautogui.size())
-width, height = pyautogui.size()
+for i in range(10):
+    pyautogui.moveTo(100, 100, duration=0.25)
+    pyautogui.moveTo(200, 100, duration=0.25)
+    pyautogui.moveTo(200, 200, duration=0.25)
+    pyautogui.moveTo(100, 200, duration=0.25)
