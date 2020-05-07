fedb253993dd5727eb1d42e6734205679137c147
syedmeesamali <syedmeesamali@yahoo.com>
2020-05-08 02:43:36 +0400

    Combinations and itertools to make first attempt to solve it

--- 2_Homeworks/Algo-2.py
+++ 2_Homeworks/Algo-2.py
@@ -1,6 +1,9 @@
-import itertools
-
-nums = range(1,25)
-chose1 = 19
-result = [seq for i in range(len(nums), 0, -1) for seq in itertools.combinations(nums,i) if sum(seq) == chose1]
-print(result)
+from itertools import combinations
+#range(-25,25,1)
+my_list = [-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
+##combinations = itertools.combinations(my_list, 3)
+##print [result for result in combinations if sum(result) == 10]
+for i in range(1, len(my_list)+1):
+    for comb in combinations(my_list, 3):
+        if sum(comb) == 10:
+            print(comb)
