5e503ec03cadc881e60d1047ee7a15a1d49d1dff
syedmeesamali <syedmeesamali@yahoo.com>
2020-05-10 05:26:04 +0400

    Some more effort with the algo

--- 2_Homeworks/Algo-2.py
+++ 2_Homeworks/Algo-2.py
@@ -1,9 +1,30 @@
+from typing import Tuple
 from itertools import combinations
-#range(-25,25,1)
-my_list = [-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
-##combinations = itertools.combinations(my_list, 3)
-##print [result for result in combinations if sum(result) == 10]
-for i in range(1, len(my_list)+1):
-    for comb in combinations(my_list, 3):
-        if sum(comb) == 19:
-            print(comb)
+
+##my_list1 = [-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
+##my_list2 = [-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
+##my_list3 = [-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
+
+my_list1 = [0,1,2,3,4,5,6,7,8,9,10]
+my_list2 = [0,1,2,3,4,5,6,7,8,9,10]
+my_list3 = [0,1,2,3,4,5,6,7,8,9,10]
+
+def sum1(a):
+    return a[0] + a[1] - a[2]
+
+def sum2(a):
+    return a[0] * a[1] + a[2]
+
+def sum3(a):
+    return a[0] + a[1] * a[2]
+
+for i in range(1, len(my_list1)+1):
+    for comb in combinations(my_list1, 3):
+        for comb2 in combinations(my_list2, 3):
+            for comb3 in combinations(my_list3, 3):
+                if sum1(comb3) == 15 and sum2(comb2) == 23 and sum3(comb1) == 50:# and sum2(comb3) == 10:
+                    print("Winning Combo")
+                    print(comb1)
+                    print(comb2)
+                    print(comb3)
+            
