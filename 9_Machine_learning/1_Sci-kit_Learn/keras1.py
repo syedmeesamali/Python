e1561bc3af93ead5e5c3629adef191df283ac9cf
syedmeesamali <syedmeesamali@yahoo.com>
2020-07-18 23:38:25 +0400

    Model training and testing

--- 9_Machine_learning/1_Sci-kit_Learn/keras1.py
+++ 9_Machine_learning/1_Sci-kit_Learn/keras1.py
@@ -12,4 +12,27 @@ concrete_data_columns = concrete_data.columns
 predictors = concrete_data[concrete_data_columns[concrete_data_columns != 'Strength']] # all columns except Strength
 target = concrete_data['Strength'] # Strength column
 
-print(predictors.head())
+#print(predictors.head())
+print(target.head())
+predictors_norm = (predictors - predictors.mean()) / predictors.std()
+predictors_norm.head()
+
+n_cols = predictors_norm.shape[1] # number of predictors
+
+#Define the regression model
+def regression_model():
+    #Create the model
+    model = Sequential()
+    model.add(Dense(50, activation='relu', input_shape = (n_cols,)))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(1))
+
+    #Compile the model
+    model.compile(optimizer = 'adam', loss = 'mean_squared_error')
+    return model
+
+#Train and Test the models
+
+#Compile the model
+model = regression_model()
+model.fit(predictors_norm, target, validation_split = 0.3, epochs = 100, verbose = 2)
