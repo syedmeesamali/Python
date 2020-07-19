ba451759b10596d50d886ce596bdb1b647921e9c
syedmeesamali <syedmeesamali@yahoo.com>
2020-07-19 23:44:34 +0400

    Second type of CNN model made and tested - need to see results (nothing visual so far :( )

--- 9_Machine_learning/1_Sci-kit_Learn/keras2.py
+++ 9_Machine_learning/1_Sci-kit_Learn/keras2.py
@@ -46,12 +46,36 @@ def convolutional_model():
     model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
     return model
 
-#Let's build the model
-model = convolutional_model()
+
+#Convolutional Layer with two sets of convolutional and pooling layers
+#Let's redefine our convolutional model so that it has two convolutional and 
+#pooling layers instead of just one layer of each.
+def convolutional_model2():
+    #Create the model
+    model = Sequential()
+    model.add(Conv2D(16, (5,5), activation = 'relu', input_shape = (28, 28, 1)))
+    model.add(MaxPooling2D(pool_size = (2,2), strides=(2,2)))
+
+    model.add(Conv2D(8, (5,5), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size = (2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(100, activation = 'relu'))
+    model.add(Dense(num_classes, activation = 'softmax'))
+
+    #compile the model
+    model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+    return model
+
+
+#Let's build the model as per second type of convolutional model
+model = convolutional_model2()
 
 #Fit the model
 model.fit(X_train, y_train, validation_data = (X_test, y_test), epochs = 10, batch_size = 200, verbose = 2)
 
 #Evaluate the model
 scores = model.evaluate(X_test, y_test, verbose = 0)
-print("Accuracy: {} \n Error: {}".format(scores[1], 100-scores[1]*100))
+print("Accuracy: {} \n Error: {}".format(scores[1], 100 - scores[1]*100))
+
+
