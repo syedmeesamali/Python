e2e698cff60cd1c68e52078dacf1d4da480c4789
syedmeesamali <syedmeesamali@yahoo.com>
2020-10-29 21:40:37 +0400

    Excellent - first full program working perfectly with command line arguments for input images

--- 9_Machine_learning/3_OpenCV/cv-1.py
+++ 9_Machine_learning/3_OpenCV/cv-1.py
@@ -25,3 +25,22 @@ print("[INFO] computing object detections ...")
 net.setInput(blob)
 detections = net.forward()
 
+#loop over the detections
+for i in range(0, detections.shape[2]):
+        #extract confidence associated with the prediction
+        confidence = detections[0, 0, i, 2]
+
+        #filter out weak confidence
+        if confidence > args["confidence"]:
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+
+                #draw the bounding box for the detected face / shape
+                text = "{:.2f}%".format(confidence * 100)
+                y = startY - 10 if startY - 10 > 10 else startY + 10
+                cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+                cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_COMPLEX, 0.45, (0, 0, 255), 2)
+
+#show the output image
+cv2.imshow("Output", image)
+cv2.waitKey(0)
