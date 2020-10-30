270bdc991b35c8d1b891dacc451bd4f6438fd8f0
syedmeesamali <syedmeesamali@yahoo.com>
2020-10-30 23:41:32 +0400

    Excellent live video feed face detector - Excellent success and fun

--- 9_Machine_learning/3_OpenCV/cv-2_Cam.py
+++ 9_Machine_learning/3_OpenCV/cv-2_Cam.py
@@ -25,4 +25,38 @@ time.sleep(2.0)
 while True:
     frame = vs.read()
     frame = imutils.resize(frame, width = 400)
+
+    #Grab frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+
+    #Pass the blob through neural network and obtain detections and predictions
+    net.setInput(blob)
+    detections = net.forward()
+
+    #Loop over the detections
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        #filter for confidence greater than minimum
+        if confidence < args["confidence"]:
+            continue
+
+        #computer (x,y) of the bounding box
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+
+        #draw bounding box along with associated probability
+        text = "{:.2f}%".format(confidence * 100)
+        y = startY - 10 if startY - 10 > 10 else startY + 10
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+        cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+        #show the output frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        #if q was pressed then break from loop
     
+#do the cleanup
+cv2.destroyAllWindows()
+vs.stop()
