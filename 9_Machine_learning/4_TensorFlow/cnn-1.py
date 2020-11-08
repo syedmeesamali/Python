020df866eed933dd4eb3fbb24a134218f3b11bef
syedmeesamali <syedmeesamali@yahoo.com>
2020-11-08 23:03:11 +0400

    Multiple neurons - most basic form

--- 9_Machine_learning/4_TensorFlow/cnn-1.py
+++ 9_Machine_learning/4_TensorFlow/cnn-1.py
@@ -1,14 +1,13 @@
 #This is a simple exercise to build neurons and then neural networks from ground-up
+inputs = [1, 2, 3, 2.5]
+weights1 = [0.2, 0.8, -0.5, 1]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+bias1 = 2            #every neuron has unique bias
+bias2 = 3
+bias3 = 0.5
+output = [inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
+          inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
+          inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3]
 
-inputs = [2.1, 5.2, 3.5]
-weights = [3.1, 2.5, 4.7]
-bias = 3            #every neuron has unique bias
-
-output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
-#print(output)
-
-inputs = [1, 2, 3]
-weights = [0.2, 0.8, -0.5]
-bias = 2            #every neuron has unique bias
-output2 = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
-print(output2)
+print(output)
