c7137660f496620ecaf547e3737dc6b4f6402e1c
syedmeesamali <syedmeesamali@yahoo.com>
2020-11-13 07:30:36 +0400

    Resizing the video frames

--- 9_Machine_learning/3_OpenCV/cv-3.py
+++ 9_Machine_learning/3_OpenCV/cv-3.py
@@ -1,15 +1,23 @@
 #From YT OpenCV course referred from "FreeCodeCamp" - Quincy
 import cv2 as cv
 
-capture = cv.VideoCapture('./rsrc/drones.mp4')
+img = cv.imread('rsrc/boss.JPG')
 
+def rescaleFrame(frame, scale = 0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+
+capture = cv.VideoCapture('./rsrc/drones.mp4')
 while True:
     isTrue, frame = capture.read()
+    frame_resized = rescaleFrame(frame)
     cv.imshow('Video', frame)
-
+    cv.imshow('Resized frames', frame_resized)
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
-
 
 capture.release()
 cv.destroyAllWindows()
