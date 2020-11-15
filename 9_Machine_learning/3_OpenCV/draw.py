81366ef9734fc6bb344761831c7367a14c2f59fd
syedmeesamali <syedmeesamali@yahoo.com>
2020-11-15 04:26:12 +0400

    Simple gradient descent using one neuron - Also some drawing using OpenCV

--- 9_Machine_learning/3_OpenCV/draw.py
+++ 9_Machine_learning/3_OpenCV/draw.py
@@ -3,10 +3,11 @@ import cv2 as cv
 import numpy as np
 
 blank = np.zeros((500, 500, 3), dtype = 'uint8')
-cv.imshow('blank', blank)
 
 #Paint certain color on the image
-blank[:] = 0, 255, 0        #Green color code
-cv.imshow('Green', blank)
+#blank[200:300, 300:400] = 0, 255, 0        #Green color code
+#cv.imshow('Green', blank)
 
+cv.rectangle(blank, (0,0), (250, 250), (0, 0, 255), thickness = 2)
+cv.imshow('Rectangle', blank)
 cv.waitKey(0)
