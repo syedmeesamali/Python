30d197e6bd984b29626d02ccaa0714696c155bb7
syedmeesamali <syedmeesamali@yahoo.com>
2020-11-25 23:47:57 +0400

    Various openCV functions

--- 9_Machine_learning/3_OpenCV/basics.py
+++ 9_Machine_learning/3_OpenCV/basics.py
@@ -7,6 +7,17 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 #cv.imshow('Gray', gray)
 
 blur = cv.GaussianBlur(img, (5,5), cv.BORDER_DEFAULT)
-cv.imshow('Blur', blur)
+#cv.imshow('Blur', blur)
+
+canny = cv.Canny(blur, 125, 175)
+#cv.imshow('Canny', canny)
+
+#Dilating the images
+dilated = cv.dilate(canny, (4,4), iterations=3)      #Kernel size of 3 x 3
+#cv.imshow('Dilated', dilated)
+
+#Resizing the images as well as cropping
+resized = cv.resize(img, (500, 500))
+cv.imshow('Resized', resized)
 
 cv.waitKey(0)
