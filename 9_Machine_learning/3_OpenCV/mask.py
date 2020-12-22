545508b4e3b15b6ab42de635d1d4c345e1a2efdf
syedmeesamali <syedmeesamali@yahoo.com>
2020-12-22 21:54:48 +0400

    Simple rectangular mask

--- 9_Machine_learning/3_OpenCV/mask.py
+++ 9_Machine_learning/3_OpenCV/mask.py
@@ -9,13 +9,13 @@ img = cv.imread('rsrc/boss.JPG')
 blank = np.zeros(img.shape[:2], dtype = 'uint8')
 #cv.imshow('Blank', blank)
 
-mask = cv.circle(blank, (img.shape[1]//2 + 150, img.shape[0] // 2), 100, 255, -1)
-mask2 = cv.rectangle(blank, (img.shape[1]//2 + 150, img.shape[0] // 2), (img.shape[1]//2 + 150, img.shape[0] // 2), 255, -1)
+#mask = cv.circle(blank, (img.shape[1]//2 + 150, img.shape[0] // 2), 100, 255, -1)
+mask2 = cv.rectangle(blank, (img.shape[1]//2 + 150, img.shape[0] // 2), (((img.shape[1]//2) + 300), ((img.shape[0] // 2) + 300)), 255, -1)
 #cv.imshow('Blank', mask)
 
-masked = cv.bitwise_and(img, img, mask = mask)
+#masked = cv.bitwise_and(img, img, mask = mask)
 masked2 = cv.bitwise_and(img, img, mask = mask2)
-cv.imshow('Masked', masked)
+#cv.imshow('Masked', masked)
 cv.imshow('Masked2', masked2)
 
 
