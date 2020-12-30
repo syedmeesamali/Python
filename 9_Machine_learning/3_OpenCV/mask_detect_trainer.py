f0354a1f43e4a0df957c34dc6851e8cdb576f6df
syedmeesamali <syedmeesamali@yahoo.com>
2020-12-30 17:33:48 +0400

    mask detector and the main file study regarding the same as of now
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 9_Machine_learning/3_OpenCV/mask_detect_trainer.py
+++ 9_Machine_learning/3_OpenCV/mask_detect_trainer.py
@@ -10,4 +10,23 @@ target = np.load('target.npy')
 
 model = Sequetial()
 
+model.add(Conv2D(100, (3,3), input_shape = data.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(100, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(2, activation='softmax'))
 
+
+
+model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+train_data, test_data, train_target, test_target = train_test_split(data, target, test_size = 0.1)
+checkpoint = ModelCheckpoint(
+    'model-{epoch:03d}.model', monitor = 'val_loss', verbose = 0, save_best_only= True, mode = 'auto')
+history = model.fit(train_data, train_target, epochs = 20, callbacks = [checkpoint], validation_split = 0.2)
+
+print(model.evaluate(test_data, test_target))
