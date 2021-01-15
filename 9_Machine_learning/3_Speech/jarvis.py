83e99696ee0e3f1edb5109993a854d6f8f9fbd26
syedmeesamali <syedmeesamali@yahoo.com>
2021-01-15 22:59:51 +0400

    Speech recognition module also getting added and will be super fun

--- 9_Machine_learning/3_Speech/jarvis.py
+++ 9_Machine_learning/3_Speech/jarvis.py
@@ -1,5 +1,29 @@
 import pyttsx3
+import datetime
 
-engine = pyttsx3.init()
-engine.say("Hello World. So happy to be able to talk. ")
-engine.runAndWait()
+engine = pyttsx3.init()         #Main speech engine
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+#speak("Hello ali how are you?")
+
+def time_():
+    Time = datetime.datetime.now().strftime("%H:%M:%S")     #24-hour time format
+    speak("The current time is: ")
+    speak(Time)
+
+#time_()
+
+def date_():
+    year = datetime.datetime.now().year
+    month = datetime.datetime.now().month
+    day = datetime.datetime.now().day
+    speak("The current date is: ")
+    speak("Year " + str(year))
+    speak("Month" + str(month))
+    speak("Day" + str(day))
+
+date_()
