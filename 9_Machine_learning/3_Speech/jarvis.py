76dc522e38038d5a34364628ead49b263d0362b7
syedmeesamali <syedmeesamali@yahoo.com>
2021-01-17 01:48:52 +0400

    Speech engine working perfectly now

--- 9_Machine_learning/3_Speech/jarvis.py
+++ 9_Machine_learning/3_Speech/jarvis.py
@@ -43,17 +43,23 @@ def greetme():
 #Run the greetme function
 #greetme()
 
+#pip install pipwin
+#pipwin install pyaudio
+
 def TakeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening .....")
         r.pause_threshold = 1
         audio = r.listen(source)
-    
     try:
         print("Recognizing....")
         query = r.recognize_google(audio, language='en-US')
         print(query)
+        if query == "hello":
+            greetme()
+        else:
+            pass
     except Exception as e:
         print(e)
         print("Say that again please.....")
