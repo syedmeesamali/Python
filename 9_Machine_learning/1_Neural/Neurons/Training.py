b23fa4089f43df60fa19a7890b73363c81b77147
syedmeesamali <syedmeesamali@yahoo.com>
2021-01-24 23:50:11 +0400

    Some updates to training algo and study of same

--- 9_Machine_learning/1_Neural/Neurons/Training.py
+++ 9_Machine_learning/1_Neural/Neurons/Training.py
@@ -71,6 +71,30 @@ class OurNeuralNetwork:
                 # -- Naming d_L / d_w1 is represented as "partial L / partial w1"
                 d_L_d_ypred = -2 * (y_true - y_pred)
 
+                #Neuron o1
+                d_ypred_d_w5 = h1 * deriv_sigmoid(sum_o1)
+                d_ypred_d_w6 = h2 * deriv_sigmoid(sum_o1)
+                d_ypred_d_b3 = deriv_sigmoid(sum_o1)
+
+                d_ypred_d_h1 = self.w5 * deriv_sigmoid(sum_o1)
+                d_ypred_d_h2 = self.w6 * deriv_sigmoid(sum_o1)
+
+                #Neuron h1
+                d_ypred_d_w1 = x[0] * deriv_sigmoid(sum_h1)
+                d_ypred_d_w2 = x[1] * deriv_sigmoid(sum_h1)
+                d_ypred_d_b1 = deriv_sigmoid(sum_h1)
+
+                #Neuron h2
+                d_ypred_d_w3 = x[0] * deriv_sigmoid(sum_h2)
+                d_ypred_d_w4 = x[1] * deriv_sigmoid(sum_h2)
+                d_ypred_d_b2 = deriv_sigmoid(sum_h2)
+
+                #Update the weights and biases
+                #Neuron h1
+                self.w1 = learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w1
+                
+
+
 #Mean square error function for training
 def mse_loss(y_true, y_pred):
     #y_true and y_pred are numpy arrays of same length
