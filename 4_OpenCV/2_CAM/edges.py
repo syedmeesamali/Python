395bea618fdc506e1f9b7bc25d984f91cb5aa702
syedmeesamali <syedmeesamali@yahoo.com>
2021-04-15 23:39:00 +0500

    xxxxxxxxxx

--- 4_OpenCV/2_CAM/edges.py
+++ 4_OpenCV/2_CAM/edges.py
@@ -2,6 +2,15 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('rsrc/boss.jpg')
-cv.imshow('value', img)
+img = cv.imread('../rsrc/boss.JPG')
+cv.imshow('image-1', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('gray', gray)
+
+#Laplacion transform for such image
+lap = cv.Laplacian(gray, cv.CV_64F)
+lap = np.uint8(np.absolute(lap))
+
 cv.waitKey(0)
+cv.destroyAllWindows()
