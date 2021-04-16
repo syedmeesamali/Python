5feb0d0867f54d8ad34d6079a1899b447d0d83d3
syedmeesamali <syedmeesamali@yahoo.com>
2021-04-16 23:52:47 +0500

    Sobel for edge detection

--- 4_OpenCV/2_CAM/edges.py
+++ 4_OpenCV/2_CAM/edges.py
@@ -3,7 +3,7 @@ import cv2 as cv
 import numpy as np
 
 img = cv.imread('../rsrc/boss.JPG')
-cv.imshow('image-1', img)
+#cv.imshow('image-1', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('gray', gray)
@@ -11,6 +11,18 @@ cv.imshow('gray', gray)
 #Laplacion transform for such image
 lap = cv.Laplacian(gray, cv.CV_64F)
 lap = np.uint8(np.absolute(lap))
+cv.imshow('Laplacion', lap)
+
+#Sobel
+sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
+sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
+
+#Combine the sobels
+combined_sobel = cv.bitwise_or(sobelx, sobely)
+
+cv.imshow('Sobel X', sobelx)
+cv.imshow('Sobel Y', sobely)
+cv.imshow('Combined', combined_sobel)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
