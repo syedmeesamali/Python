63413d48946d741877cca6b6f2c2e37b0c4baffe
syedmeesamali <syedmeesamali@yahoo.com>
2021-05-21 22:43:37 +0400

    Classification boundary - FastAI course

--- 15_Misc/make_html.py
+++ 15_Misc/make_html.py
@@ -4,4 +4,4 @@ with open("all.html", "w") as a:
     for path, subdirs, files in os.walk(r'C:\Users\SYED\Downloads\New folder'):
        for filename in files:
          f = os.path.join(path, filename)
-         a.write("<img src=\"" + str(f) + "\"" + " style='width:1200px;height:900px;/^>" + os.linesep) 
+         a.write("<img src=\"" + str(f) + "\" /^>" + os.linesep) 
