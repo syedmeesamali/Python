1ea47a4262a987642c5da45c66d2f1183b87e361
syedmeesamali <syedmeesamali@yahoo.com>
2021-06-30 16:40:00 +0400

    pixel comparison is indirect way to measure crack lengths by making reference to actual area measurements
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 4_OpenCV/5_Red_Color/pixels.py
+++ 4_OpenCV/5_Red_Color/pixels.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
-from PIL import Image
-
-im = np.array(Image.open("map1.png").convert('RGB'))
-sought = [255, 0, 0]
-result = np.count_nonzero(np.all(im == sought, axis = 2))
-print(result)
+im = cv2.imread('small.bmp')
+whites = [255, 255, 255]                #Always counted as B - G - R not RGB
+reds = [0, 0, 255]                #Always counted as B - G - R not RGB
+result_white = np.count_nonzero(np.all(im == whites, axis = 2))
+result_red = np.count_nonzero(np.all(im == reds, axis = 2))
+print(f"White pixels are {result_white}")
+print(f"Red pixels are {result_red}")
