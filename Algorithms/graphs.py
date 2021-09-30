5158c1980c1debde22cc8dc437f303dea22452f1
syedmeesamali <syedmeesamali@yahoo.com>
2021-09-30 12:42:39 +0400

    Amazingly perfectly done A* algorithm main implementation - looks like a fun search game

--- Algorithms/graphs.py
+++ Algorithms/graphs.py
@@ -72,7 +72,23 @@ class Spot:
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
     
     def update_neighbors(self, grid):
-        pass
+        self.neighbors = []
+        
+        #Check DOWN a row if it is LAST row of grids of not - can't go out of game layout
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier(): 
+            self.neighbors.append(grid[self.row + 1][self.col])
+        
+        #Check UP a row
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():
+            self.neighbors.append(grid[self.row - 1][self.col])
+        
+        #Check RIGHT a row
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():
+            self.neighbors.append(grid[self.row][self.col + 1])
+        
+        #Check LEFT a row
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():
+            self.neighbors.append(grid[self.row][self.col - 1])
 
     def __lt__(self, other):
         return False
@@ -82,6 +98,47 @@ def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)      #Distance function
+
+#Actual alogirthm for actual updates
+def algorithm(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+    g_score = {spot: float("inf") for row in grid for spot in row}
+    g_score[start] = 0
+    f_score = {spot: float("inf") for row in grid for spot in row}      #Based on some heuristics
+    f_score[start] = h(start.get_pos(), end.get_pos())
+
+    open_set_hash = {start}
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+
+        if current == end:
+            return True                 #shortest path found
+        
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+        
+        draw()
+        if current != start:
+            current.make_closed()
+
+    return False
 
 #Make the logical grid
 def make_grid(rows, width):
@@ -158,7 +215,11 @@ def main(win, width):
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and not started:
-                    pass
+                    for row in grid:
+                        for spot in row:
+                            spot.update_neighbors(grid)
+                    
+                    algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
 
     pygame.quit()       #If cross is clicked
 
