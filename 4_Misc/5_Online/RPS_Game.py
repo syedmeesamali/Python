5ab1de0557211c8ae2664e5c092913238083fa3f
syedmeesamali <syedmeesamali@yahoo.com>
2022-11-12 23:42:17 +0500

    rock-paper-scissor game

---
+++ 4_Misc/5_Online/RPS_Game.py
@@ -0,0 +1,29 @@
+def name_to_number(num):
+    if num == "rock":
+        return 0
+    elif num == "Spock":
+        return 1
+    elif num == "paper":
+        return 2
+    elif num == "lizard":
+        return 3
+    elif num == "scissors":
+        return 4
+    else:
+        return "ERROR!"
+
+print(name_to_number("lizard"))
+
+def number_to_name(name):
+    if name == "rock":
+        return 0
+    elif name == "Spock":
+        return 1
+    elif name == "paper":
+        return 2
+    elif name == "lizard":
+        return 3
+    elif name == "scissors":
+        return 4
+    else:
+        return "ERROR!"
