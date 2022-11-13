1e49031cdda2e60c2cfed638d5077a4a09355410
syedmeesamali <syedmeesamali@yahoo.com>
2022-11-13 22:18:50 +0500

    Need to work on test messages - almost done final

---
+++ 4_Misc/5_Online/final_project.py
@@ -0,0 +1,73 @@
+import datetime as dt
+def days_in_month(year, month):
+    """
+    Inputs:
+      year  - an integer between datetime.MINYEAR and datetime.MAXYEAR
+              representing the year
+      month - an integer between 1 and 12 representing the month
+      
+    Returns:
+      The number of days in the input month.
+    """
+
+def is_valid_date(year, month, day):
+    """
+    Inputs:
+      year  - an integer representing the year
+      month - an integer representing the month
+      day   - an integer representing the day
+      
+    Returns:
+      True if year-month-day is a valid date and
+      False otherwise
+    """
+    if year <= dt.MAXYEAR and year >= dt.MINYEAR:
+        if month > 0 and month <= 12:
+            if day > 0 and day <= 31:
+                return True
+    else:
+        return "ERROR!"
+
+def days_between(year1, month1, day1, year2, month2, day2):
+    """
+    Inputs:
+      year1  - an integer representing the year of the first date
+      month1 - an integer representing the month of the first date
+      day1   - an integer representing the day of the first date
+      year2  - an integer representing the year of the second date
+      month2 - an integer representing the month of the second date
+      day2   - an integer representing the day of the second date
+      
+    Returns:
+      The number of days from the first date to the second date.
+      Returns 0 if either date is invalid or the second date is 
+      before the first date.
+    """
+    if year2 >= year1 and month2 >= month1 and day2 >= day1:
+        delta = dt.date(year2, month2, day2) - dt.date(year1, month1, day1)
+        return delta.days
+    else:
+        return 0
+
+def age_in_days(year, month, day):
+    """
+    Inputs:
+      year  - an integer representing the birthday year
+      month - an integer representing the birthday month
+      day   - an integer representing the birthday day
+      
+    Returns:
+      The age of a person with the input birthday as of today.
+      Returns 0 if the input date is invalid of if the input
+      date is in the future.
+    """
+    year_now = dt.date.today().year
+    month_now = dt.date.today().month
+    day_now = dt.date.today().day
+
+    if is_valid_date(year, month, day):
+        return days_between(year, month, day, year_now, month_now, day_now)
+    else:
+        return 0
+
+#print(age_in_days(1986, 6, 1))
