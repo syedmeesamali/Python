5c7d7825babea87364b56b561a8e9557a488ae51
syedmeesamali <syedmeesamali@yahoo.com>
2022-11-13 13:51:15 +0500

    rock-paper game main part

--- 4_Misc/5_Online/RPS_Game.py
+++ 4_Misc/5_Online/RPS_Game.py
@@ -12,18 +12,31 @@ def name_to_number(num):
     else:
         return "ERROR!"
 
-print(name_to_number("lizard"))
-
-def number_to_name(name):
-    if name == "rock":
-        return 0
-    elif name == "Spock":
-        return 1
-    elif name == "paper":
-        return 2
-    elif name == "lizard":
-        return 3
-    elif name == "scissors":
-        return 4
+def number_to_name(numb):
+    if numb == 0:
+        return "rock"
+    elif numb == 1:
+        return "Spock"
+    elif numb == 2:
+        return "paper"
+    elif numb == 3:
+        return "lizard"
+    elif numb == 4:
+        return "scissors"
     else:
         return "ERROR!"
+
+def rpsls(player_choice):
+    print()
+    if player_choice == 1:
+        print("You choose Name to Number Game!")
+        x = input("Enter name? ")
+        print(name_to_number(x))
+    elif player_choice == 2:
+        print("You choose Number to Name Game!")
+        x = input("Enter number? ")
+        print(number_to_name(int(x)))
+    else:
+        print("Bye have a good day!")
+n = int(input("Enter choice.. "))
+rpsls(n)
