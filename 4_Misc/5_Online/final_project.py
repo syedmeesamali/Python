02b870ada6b00ace9da6225c400ccd9ae543fac2
syedmeesamali <syedmeesamali@yahoo.com>
2022-11-14 15:05:53 +0500

    Final project  still ongoing

--- 4_Misc/5_Online/final_project.py
+++ 4_Misc/5_Online/final_project.py
@@ -9,6 +9,13 @@ def days_in_month(year, month):
     Returns:
       The number of days in the input month.
     """
+    if month in {1, 3, 5, 7, 8, 10, 12}:
+        return 31
+    if month == 2:
+        if leap_year(year):
+            return 29
+        return 28
+    return 30
 
 def is_valid_date(year, month, day):
     """
@@ -21,12 +28,24 @@ def is_valid_date(year, month, day):
       True if year-month-day is a valid date and
       False otherwise
     """
-    if year <= dt.MAXYEAR and year >= dt.MINYEAR:
-        if month > 0 and month <= 12:
-            if day > 0 and day <= 31:
-                return True
-    else:
+    correctDate = None
+    try:
+        newDate = dt.datetime(year, month, day)
+        correctDate = True
+        return True
+    except ValueError:
+        correctDate = False
         return False
+    #return correctDate
+
+def leap_year(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
 
 def days_between(year1, month1, day1, year2, month2, day2):
     """
@@ -43,16 +62,18 @@ def days_between(year1, month1, day1, year2, month2, day2):
       Returns 0 if either date is invalid or the second date is 
       before the first date.
     """
-    if year2 >= year1 or (year2 >= year1 and month2 >= month1) or (year2 >= year1 and month2 >= month1 and day2 >= day1):
-        delta = dt.date(year2, month2, day2) - dt.date(year1, month1, day1)
+    if (year2 >= year1 and month2 >= month1 and day2 >= day1) or (year2 >= year1 and month2 >= month1) or (year2 >= year1):
+        d1 = dt.date(year1, month1, day1)
+        d2 = dt.date(year2, month2, day2)
+        #delta = (dt.date(year2, month2, day2) - dt.date(year1, month1, day1))
+        delta = d2 - d1
+        #print("Its okay diff")
         return delta.days
-    elif year1 > year2:
-        return 0
-    elif year1 < year2 and month1 > month2:
-        return 0
-    elif year1 < year2 and month1 < month2 and day1 > day2:
+    elif (year2 < year1) or (year2 < year1 and month2 < month1) or (year2 < year1 and month2 < month1 and day2 < day1):
+        #print("Wrong diff 1")
         return 0
     else:
+        #print("Wrong diff 0")
         return 0
 
 def age_in_days(year, month, day):
@@ -73,11 +94,12 @@ def age_in_days(year, month, day):
     print(str(year_now) + " _ " + str(month_now) + " _ " + str(day_now))
 
     if is_valid_date(year, month, day):
-        print("Date is valid")
+        #print("Date is valid")
         return days_between(year, month, day, year_now, month_now, day_now)
     else:
         return 0
 
-print(age_in_days(2016, 12, 31))
-print(days_between(1973, 8, 14, 1973, 8, 13))
-print(is_valid_date(0, 1, 1))
+#print(age_in_days(2016, 12, 31))
+#print(days_between(2017, 12, 31, 2018, 1, 1))
+#print(is_valid_date(2017, 2, 28))
+#print(days_in_month(2005, 2))
