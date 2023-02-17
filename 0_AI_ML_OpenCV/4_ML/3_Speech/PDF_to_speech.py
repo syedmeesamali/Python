e726ff02c5886208498339e1850674137c57f297
syedmeesamali <syedmeesamali@yahoo.com>
2023-02-17 22:27:04 +0400

    Layout and some folder arrangement updates

---
+++ 0_AI_ML_OpenCV/4_ML/3_Speech/PDF_to_speech.py
@@ -0,0 +1,5 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.say("This is working excellently now and should or can be used with ChatGPT")
+engine.runAndWait()
